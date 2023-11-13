eaa7dd98ba538ed995b8226ead41f454c8e185ba
Avishek Majumder <133078002+avi-xcc@users.noreply.github.com>
2023-11-13 11:10:55 +0800

    Update chroma_request.py
    Display fetch results in a structured way

--- chroma_request.py
+++ chroma_request.py
@@ -7,11 +7,13 @@ client = chromadb.PersistentClient(path="./docs_cache/")
 
 collection = client.get_or_create_collection(name="pdf_books", embedding_function=sentence_transformer_ef)
 
-print(collection.peek())
-query = "What is Call options?"
+query = "What is a class?"
 
 fetched_results = collection.query(query_texts=[query], n_results=3)
 
-
-print(fetched_results)
-
+# The data structure of the returned result is a dictionary, with lists. We fetch the distance
+# and the documents. Then we select the matches for the first query (since we only have one
+# query, this list of length 1). Then we iterate through the 3 results.
+for dist, doc in zip(fetched_results['distances'][0], fetched_results['documents'][0]):
+    print(f"DISTANCE: {dist}\n{doc}")
+    print('=' * 25)
