cd561abcd87934b5cad2a46dade12c21a6e289d4
Avishek Majumder <133078002+avi-xcc@users.noreply.github.com>
2023-11-13 11:10:36 +0800

    Update chroma_client.py
    Added outputs for better visibility

--- chroma_client.py
+++ chroma_client.py
@@ -1,17 +1,17 @@
 import chromadb
 from chromadb.utils import embedding_functions
 from PyPDF2 import PdfReader
+import unicodedata
 from tqdm import tqdm
 
-
-pdf_path = r"C:\Documents\Trading-Volatility.pdf"
+pdf_path = "./python_book_01.pdf"
 reader = PdfReader(pdf_path)
 # page = reader.pages[100]
 # print(type(page.extract_text()))
 
-sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="paraphrase-albert-small-v2")
+sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
+    model_name="paraphrase-albert-small-v2")
 client = chromadb.PersistentClient(path="./docs_cache/")
-
 
 collection = client.get_or_create_collection(name="pdf_books", embedding_function=sentence_transformer_ef)
 doc_list = []
@@ -19,12 +19,15 @@ metadata_list = []
 id_list = []
 
 for pageno, page in tqdm(enumerate(reader.pages)):
-	doc_list.append(page.extract_text())
-	metadata_list.append({"reference": "Trading_Volatility_{pageno + 1}"})
-	id_list.append(str(pageno))
+    # This is required to normalize the text to unicode data
+    new_str = unicodedata.normalize("NFKD", page.extract_text())
+    doc_list.append(new_str)
+    metadata_list.append({"reference": f"python_{pageno + 1}"})
+    id_list.append(str(pageno))
 
+print(f"Adding {len(doc_list)} to the collection.")
 collection.add(documents=doc_list,
-	metadatas=metadata_list,
-	ids=id_list)
+               metadatas=metadata_list,
+               ids=id_list)
 
-print(collection.count())
+print(f"There are {collection.count()} documents in the collection.")
